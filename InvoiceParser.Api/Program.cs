e80d80774c5accbdbd65e79bab8036e8ef6db18f
Vagelis Manousakis <vag_manousakis@hotmail.com>
2025-12-30 18:37:25 +0200

    make it https

--- InvoiceParser.Api/Program.cs
+++ InvoiceParser.Api/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddSingleton<InvoiceParser.Api.Services.IEntersSoftIframeFetche
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.WebHost.UseUrls("http://0.0.0.0:7254");
+//builder.WebHost.UseUrls("http://0.0.0.0:7254");
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
